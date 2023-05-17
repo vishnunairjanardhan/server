03dd5e25f4ff35f2d64b859c7a0f33cad90c426b
nitesh <nitesh.gaud@e-stonetech.com>
2023-05-17 17:39:57 +0530

    ruby gem commite

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem 'stripe'
 gem "httparty", "~> 0.21.0"
+gem "puma"
--- testing.rb
+++ testing.rb
@@ -2,10 +2,10 @@ require 'json'
 require 'httparty'
 apiEndpointForcarts = 'https://api.us-central1.gcp.commercetools.com/obongg26te1hxzh/carts'
 headers = {
-    'Authorization' =>'Bearer gyqgh-0_yw9R1_5ipE3hV7WzdtpicUfA'
+    'Authorization' =>'Bearer JJTvx3YvE8lxwwFvL473JALia52Mw-0W'
     }
     response = HTTParty.get(apiEndpointForcarts, headers: headers)
   pop=response.body
   ruby_hash = JSON.parse(pop)
-  arrayoflineitems=ruby_hash['results'][16]["lineItems"]
+  arrayoflineitems=ruby_hash['results'].last["lineItems"]
   puts(arrayoflineitems)
